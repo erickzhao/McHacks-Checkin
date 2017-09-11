07f061c50bc669905239cd0dbe90a6e757933698
Shreyas Kapur <shreyask@mit.edu>
2017-09-11 19:54:11 -0400

    NFC

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ The registartion system's API endpoint without trailing forward-slash
 /orders /users and /auth/login are used.
 
 */
-var API_ROOT = "https://my.jankhack.party";
+var API_ROOT = "https://my.hackmit.org";
 
 /*
 Should I use the ID scanner API?
@@ -18,3 +18,13 @@ var ENABLE_SCANNER = true;
 TechX's internal scanner API endpoint
 */
 var SCANNER_API = "http://localhost:5000/cardscanner"
+
+/*
+Endpoint for NFC
+*/
+var NFCDB_EP = 'http://localhost:5000/add_tag';
+
+/*
+Secret for NFC
+*/
+var NFCDB_SECRET = 'ihaveafewdevrequests';
